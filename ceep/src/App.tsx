0564ab0a3162305f848bcfb02cae5ddb9c350b3b
cassio <cassiorei98@gmail.com>
2022-01-11 17:05:44 -0300

    criação do projeto

---
+++ ceep/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+ <form>
+   <input type="text" placeholder="titulo"/>
+   <textarea placeholder="escreva sua nota..."/>
+   <button>Criar Nota</button>
+  </form>
+  );
+}
+
+export default App;
