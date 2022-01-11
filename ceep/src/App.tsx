78970e6290512bfe5494a2fecc1466f89b394a1a
cassio <cassiorei98@gmail.com>
2022-01-11 17:54:21 -0300

    criando react

--- ceep/src/App.tsx
+++ ceep/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-
-import './App.css';
-
+import  listaDeNotas  from "./components/ListaDeNotas";
 function App() {
   return (
+ <section>
  <form>
    <input type="text" placeholder="titulo"/>
    <textarea placeholder="escreva sua nota..."/>
    <button>Criar Nota</button>
   </form>
+   <listaDeNotas/> 
+  </section>
   );
-}
+  }
 
 export default App;
